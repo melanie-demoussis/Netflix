94b13bb185d4253c7efa24e1254f58f45f19e765
Mélanie DEMOUSSIS <melaniedemoussis@MacBook-Air-de-Melanie.local>
2023-02-01 15:55:52 +0100

    correction taille photos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import data from "./data.json";
-import image from "./img/netflix.png";
+import logo from "./img/netflix.png";
 import Section from "./components/Section";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <img src={image} alt="fd" />
+        <img className="logo" src={logo} alt="fd" />
       </header>
       {data.map((elem, num) => {
         return <Section key={num} elem={elem} />;
